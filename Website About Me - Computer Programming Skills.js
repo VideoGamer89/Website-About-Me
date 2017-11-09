c60a501576f773d3494eaef845106b6f160f5b18
Jeff Vogel <jefferyseanvogel89@gmail.com>
2017-11-09 14:35:29 -0600

    Create Website About Me - Computer Programming Skills.js

---
+++ Website About Me - Computer Programming Skills.js
@@ -0,0 +1,44 @@
+function ComputerProgrammingSkillsProcess()
+{
+  // Create HTML code
+  var string;
+  string = "<ul>"
+			+ "<li>C++</li>"
+			+ "<li>C#</li>"
+			+ "<li>Java</li>"
+			+ "<li>JavaScript</li>"
+			+ "<li>HTML (HyperText Markup Language)</li>"
+			+ "<li>CSS (Cascading Style Sheet)</li>"
+			+ "<li>AJAX (Asynchronous JavaScript and XML)</li>"
+			+ "<li>MySQL (Structure Query Language)</li>"
+			+ "<li>PHP (Personal Home Page)</li>"
+			+ "<li>F#</li>"
+			+ "<li>XML (eXtensible Markup Language)</li>"
+			+ "<li>Visual Basic</li>"
+			+ "<li>VBScript</li>"
+			+ "<li>Android Development</li>"
+			+ "<li>Web Development</li>"
+			+ "<li>Trello</li>"
+			+ "<li>Hot Jar</li>"
+			+ "<li>LearnDash</li>"
+			+ "<li>WordPress</li>"
+			+ "<li>Typeform</li>"
+			+ "<li>Tag Cloud</li>"
+			+ "<li>Google Analytics</li>"
+			+ "<li>Google Tag Manager</li>"
+			+ "<li>Gliffy</li>"
+			+ "<li>Google Sheets</li>"
+			+ "<li>Google Docs</li>"
+			+ "<li>Google Slides</li>"
+			+ "<li>Google Page Speed Insights</li>"
+			+ "<li>Webpage Test</li>"
+			+ "<li>Wikia</li>"
+			+ "<li>Gogle Chrome Developer Tools</li>"
+			+ "<li>Conversion Fly</li>"
+			+ "<li>C</li>"
+		+ "</ul>";
+    // obtain a reference to the <div>
+    myDiv = document.getElementById("myDivComputerProgrammingSkillsElement");
+    // add content to the <div> elementFromPoint
+    myDiv.innerHTML = string;
+}
