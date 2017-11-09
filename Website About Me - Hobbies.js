c65c382bde1931dbdfcbf7f1f440f211616905f3
Jeff Vogel <jefferyseanvogel89@gmail.com>
2017-11-09 14:57:51 -0600

    Create Website About Me - Hobbies.js

---
+++ Website About Me - Hobbies.js
@@ -0,0 +1,17 @@
+function HobbiesProcess()
+{
+  // Create HTML code
+  var string;
+  string = "<ul>"
+			+ "<li>Playing Video Games</li>"
+			+ "<li>Watching TV</li>"
+			+ "<li>Listening To Music</li>"
+			+ "<li>Watching Movies</li>"
+			+ "<li>Bowling</li>"
+			+ "<li>Going to the <a href=" + "http://www.wisdells.com/wisconsin-dells.htm" + ">Wisconsin Dells</a></li>"
+		+ "</ul>";
+    // obtain a reference to the <div>
+    myDiv = document.getElementById("myDivHobbiesElement");
+    // add content to the <div> element
+    myDiv.innerHTML = string;
+}
