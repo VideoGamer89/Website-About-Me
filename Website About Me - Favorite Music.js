51488b0f48da23f7b8e77da68c4286cee6768daf
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-01-30 10:18:22 -0600

    Update Website About Me - Favorite Music.js

--- Website About Me - Favorite Music.js
+++ Website About Me - Favorite Music.js
@@ -1,8 +1,8 @@
-function FavoriteMusicProcess()
+function FavoriteVideoGamesProcess()
 {
-  // Create HTML code
-  var string;
-  string = "<ol>"
+	// Create HTML code
+	var string;
+	string = "<ol>"
 			+ "<li>Hip Hop</li>"
 			+ "<li>Punk</li>"
 			+ "<li>Rock</li>"
@@ -11,8 +11,8 @@ function FavoriteMusicProcess()
 			+ "<li>Christian Metal</li>"
 			+ "<li>Contemporary Christian Music</li>"
 		+ "</ol>";
-    // obtain a reference to the <div>
-    myDiv = document.getElementById("myDivFavoriteMusicElement");
-    // add content to the <div> element
-    myDiv.innerHTML = string;
+	// obtain a reference to the <div>
+	myDiv = document.getElementById("myDivFavoriteMusicElement");
+	// add content to the <div> element
+	myDiv.innerHTML = string;
 }
