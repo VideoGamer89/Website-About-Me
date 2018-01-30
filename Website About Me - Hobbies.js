aa04e44e649d19aafc6a26751f7120629be407db
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-01-30 10:24:18 -0600

    Update Website About Me - Hobbies.js

--- Website About Me - Hobbies.js
+++ Website About Me - Hobbies.js
@@ -1,8 +1,8 @@
 function HobbiesProcess()
 {
-  // Create HTML code
-  var string;
-  string = "<ul>"
+	// Create HTML code
+	var string;
+	string = "<ul>"
 			+ "<li>Playing Video Games</li>"
 			+ "<li>Watching TV</li>"
 			+ "<li>Listening To Music</li>"
@@ -10,8 +10,8 @@ function HobbiesProcess()
 			+ "<li>Bowling</li>"
 			+ "<li>Going to the <a href=" + "http://www.wisdells.com/wisconsin-dells.htm" + ">Wisconsin Dells</a></li>"
 		+ "</ul>";
-    // obtain a reference to the <div>
-    myDiv = document.getElementById("myDivHobbiesElement");
-    // add content to the <div> element
-    myDiv.innerHTML = string;
+	// obtain a reference to the <div>
+	myDiv = document.getElementById("myDivHobbiesElement");
+	// add content to the <div> element
+	myDiv.innerHTML = string;
 }
