0d0b5ff0afde584c2e6dff964a75be5928520a96
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-01-30 10:25:45 -0600

    Update Website About Me - Work Samples.js

--- Website About Me - Work Samples.js
+++ Website About Me - Work Samples.js
@@ -1,14 +1,15 @@
 function WorkSamplesProcess()
 {
-  // Create HTML code
-  var string;
-  string = "<ul>"
+	// Create HTML code
+	var string;
+	string = "<ul>"
 			+ "<li><a href=" + "http://members.bestbusinesscoach.ca/jeff-job/" + ">My Version of The Mentoring Page</a></li>"
 			+ "<li><a href=" + "http://members.bestbusinesscoach.ca/podcast-interviews" + ">Podcast Interview Page With The Banners Containing the Business Form</a></li>"
 			+ "<li><a href=" + "http://members.bestbusinesscoach.ca/main-member-home" + ">Updated Main Member Home Page</a></li>"
-		+ "</ul>";
-    // obtain a reference to the <div>
-    myDiv = document.getElementById("myDivWorkSamplesElement");
-    // add content to the <div> element
-    myDiv.innerHTML = string;
+		+ "</ul>"
+		+ "<p>I transcribed podcast interviews and tested the Basic Business Course, ensuring absence of errors involving webpages and / or broken links.";
+	// obtain a reference to the <div>
+	myDiv = document.getElementById("myDivWorkSampleElement");
+	// add content to the <div> elementFromPoint
+	myDiv.innerHTML = string;
 }
