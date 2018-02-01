0bd9cbc4dfed23a5c45de611d903e6706827770c
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-02-01 09:22:30 -0600

    Create Website About Me - Favorite Movies.php

---
+++ Website About Me - Favorite Movies.php
@@ -0,0 +1,70 @@
+<?php
+  // get the navigation from the request
+	$favorite_movie_1 = $_GET['Deadpool (Film)'];
+	$favorite_movie_2 = $_GET['The Amazing Spider-Man (2012 Film)'];
+	$favorite_movie_3 = $_GET['Ratchet and Clank (2016 Film)'];
+	$favorite_movie_4 = $_GET['Batman V. Superman: Dawn of Justice'];
+	$favorite_movie_5 = $_GET['Batman: The Killing Joke'];
+	$favorite_movie_6 = $_GET['The Angry Birds Movie'];
+	$favorite_movie_7 = $_GET['Teenage Mutant Ninja Turtles (2014 Film)'];
+	$favorite_movie_8 = $_GET['Teenage Mutant Ninja Turtles: Out of the shadows'];
+	$favorite_movie_9 = $_GET['Warcraft (2016 Film)'];
+	$favorite_movie_10 = $_GET['Terminator: Genisys'];
+	$favorite_movie_11 = $_GET['Independence Day: Resurgence'];
+	$favorite_movie_12 = $_GET['Assassin\'s Creed (Film)'];
+	$favorite_movie_13 = $_GET['Ant Man (Film)'];
+	$favorite_movie_14 = $_GET['Ghost in the Shell (2017 Film)'];
+	$favorite_movie_15 = $_GET['Power Rangers (2017 Film)'];
+	$favorite_movie_16 = $_GET['Spider-Man: Homecoming'];
+	$favorite_movie_17 = $_GET['Star Wars: The Force Awakens'];
+	$favorite_movie_18 = $_GET['Rogue One: A Star Wars Story'];
+	$favorite_movie_19 = $_GET['Sing (2016 Film)'];
+	$favorite_movie_20 = $_GET['Wonder Woman (2017 Film)'];
+	$favorite_movie_21 = $_GET['The Passion of the Christ (2004 Film)'];
+	$favorite_movie_22 = $_GET['The Amazing Spider-Man 2 (2014 Film)'];
+	$favorite_movie_23 = $_GET['Wreck-It Ralph'];
+	$favorite_movie_24 = $_GET['Guardians of the Galaxy (2014 Film)'];
+	$favorite_movie_25 = $_GET['Guardians of the Galaxy Vol. 2 (2017 Film)'];
+	$favorite_movie_26 = $_GET['Marvel\'s Avengers (2012 Film)'];
+	$favorite_movie_27 = $_GET['Marvel\'s Avengers: Age of Ultron'];
+	$favorite_movie_28 = $_GET['Captain Americ: Civil War (2016 Film)'];
+	$favorite_movie_29 = $_GET['Captain America: The Winter Soldier (2014 Film)'];
+?>
+<?php include 'Website About Me - Favorite Movies - header.php'; ?>
+  <div class = "content">
+    <p>Here Are My Favorite Movies</p>
+    <div>
+      <ul>
+				<li><?php echo $favorite_movie_1; ?></li>
+				<li><?php echo $favorite_movie_2; ?></li>
+				<li><?php echo $favorite_movie_3; ?></li>
+				<li><?php echo $favorite_movie_4; ?></li>
+				<li><?php echo $favorite_movie_5; ?></li>
+				<li><?php echo $favorite_movie_6; ?></li>
+				<li><?php echo $favorite_movie_7; ?></li>
+				<li><?php echo $favorite_movie_8; ?></li>
+				<li><?php echo $favorite_movie_9; ?></li>
+				<li><?php echo $favorite_movie_10; ?></li>
+				<li><?php echo $favorite_movie_11; ?></li>
+				<li><?php echo $favorite_movie_12; ?></li>
+				<li><?php echo $favorite_movie_13; ?></li>
+				<li><?php echo $favorite_movie_14; ?></li>
+				<li><?php echo $favorite_movie_15; ?></li>
+				<li><?php echo $favorite_movie_16; ?></li>
+				<li><?php echo $favorite_movie_17; ?></li>
+				<li><?php echo $favorite_movie_18; ?></li>
+				<li><?php echo $favorite_movie_19; ?></li>
+				<li><?php echo $favorite_movie_20; ?></li>
+				<li><?php echo $favorite_movie_21; ?></li>
+				<li><?php echo $favorite_movie_22; ?></li>
+				<li><?php echo $favorite_movie_23; ?></li>
+				<li><?php echo $favorite_movie_24; ?></li>
+				<li><?php echo $favorite_movie_25; ?></li>
+				<li><?php echo $favorite_movie_26; ?></li>
+				<li><?php echo $favorite_movie_27; ?></li>
+				<li><?php echo $favorite_movie_28; ?></li>
+				<li><?php echo $favorite_movie_29; ?></li>
+			</ul>
+    </div>
+  </div>
+<?php include 'Website About Me - footer.php'; ?>
