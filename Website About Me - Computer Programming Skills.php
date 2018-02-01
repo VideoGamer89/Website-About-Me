89ab345cd378747be17a80f636b774c7197e3f92
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-02-01 09:05:11 -0600

    Create Website About Me - Computer Programming Skills.php

---
+++ Website About Me - Computer Programming Skills.php
@@ -0,0 +1,83 @@
+<?php
+	$computer_programming_skill_1 = $_GET['C++'];
+	$computer_programming_skill_2 = $_GET['C#'];
+	$computer_programming_skill_3 = $_GET['Java'];
+	$computer_programming_skill_4 = $_GET['JavaScript'];
+	$computer_programming_skill_5a = $_GET['HTML']
+	$computer_programming_skill_5b = $_GET['HyperText Markup Language'];
+	$computer_programming_skill_6a = $_GET['CSS'];
+	$computer_programming_skill_6b = $_GET['Cascading Style Sheet'];
+	$computer_programming_skill_7a = $_GET['AJAX'];
+	$computer_programming_skill_7b = $_GET['Asynchronous JavaScript and XML'];
+	$computer_programming_skill_8a = $_GET['MySQL'];
+	$computer_programming_skill_8b = $_GET['Structure Query Language'];
+	$computer_programming_skill_9a = $_GET['PHP'];
+	$computer_programming_skill_9b = $_GET['Personal Home Page'];
+	$computer_programming_skill_10 = $_GET['F#'];
+	$computer_programming_skill_11a = $_GET['XML'];
+	$computer_programming_skill_11b = $_GET['eXtensible Markup Language'];
+	$computer_programming_skill_12 = $_GET['Visual Basic'];
+	$computer_programming_skill_13 = $_GET['VBScript'];
+	$computer_programming_skill_14 = $_GET['Android Development'];
+	$computer_programming_skill_15 = $_GET['Web Development'];
+	$computer_programming_skill_16 = $_GET['Trello'];
+	$computer_programming_skill_17 = $_GET['Hot Jar'];
+	$computer_programming_skill_18 = $_GET['LearnDash'];
+	$computer_programming_skill_19 = $_GET['WordPress'];
+	$computer_programming_skill_20 = $_GET['Typeform'];
+	$computer_programming_skill_21 = $_GET['Tag Cloud'];
+	$computer_programming_skill_22 = $_GET['Google Analytics'];
+	$computer_programming_skill_23 = $_GET['Google Tag Manager'];
+	$computer_programming_skill_24 = $_GET['Gliffy'];
+	$computer_programming_skill_25 = $_GET['Google Sheets'];
+	$computer_programming_skill_26 = $_GET['Google Docs'];
+	$computer_programming_skill_27 = $_GET['Google Slides'];
+	$computer_programming_skill_28 = $_GET['Google Page Speed Insights'];
+	$computer_programming_skill_29 = $_GET['Webpage Test'];
+	$computer_programming_skill_30 = $_GET['Wikia'];
+	$computer_programming_skill_31 = $_GET['Google Chrome Developer Tools'];
+	$computer_programming_skill_32 = $_GET['Conversion Fly'];
+	$computer_programming_skill_33 = $_GET['C'];
+?>
+<?php include 'Website About Me - Computer Programming Skills - header.php'; ?>
+  <div class = "content">
+    <p>Here are the computer programming skills that I've learned through Pine Technical and Community College and my internship with Best Business Coach.</p>
+    <div>
+      <ul>
+				<li><?php echo $computer_programming_skill_1; ?></li>
+				<li><?php echo $computer_programming_skill_2; ?></li>
+				<li><?php echo $computer_programming_skill_3; ?></li>
+				<li><?php echo $computer_programming_skill_4; ?></li>
+				<li><?php echo $computer_programming_skill_5a; ?> (<?php echo $computer_programming_skill_5b; ?>)</li>
+				<li><?php echo $computer_programming_skill_6a; ?> (<?php echo $computer_programming_skill_6b; ?>)</li>
+				<li><?php echo $computer_programming_skill_7a; ?> (<?php echo $computer_programming_skill_7b; ?>)</li>
+				<li><?php echo $computer_programming_skill_8a; ?> (<?php echo $computer_programming_skill_8b; ?>)</li>
+				<li><?php echo $computer_programming_skill_9a; ?> (<?php echo $computer_programming_skill_9b; ?>)</li>
+				<li><?php echo $computer_programming_skill_10; ?></li>
+				<li><?php echo $computer_programming_skill_11a; ?> (<?php echo $computer_programming_skill_11b; ?>)</li>
+				<li><?php echo $computer_programming_skill_12; ?></li>
+				<li><?php echo $computer_programming_skill_13; ?></li>
+				<li><?php echo $computer_programming_skill_14; ?></li>
+				<li><?php echo $computer_programming_skill_15; ?></li>
+				<li><?php echo $computer_programming_skill_16; ?></li>
+				<li><?php echo $computer_programming_skill_17; ?></li>
+				<li><?php echo $computer_programming_skill_18; ?></li>
+				<li><?php echo $computer_programming_skill_19; ?></li>
+				<li><?php echo $computer_programming_skill_20; ?></li>
+				<li><?php echo $computer_programming_skill_21; ?></li>
+				<li><?php echo $computer_programming_skill_22; ?></li>
+				<li><?php echo $computer_programming_skill_23; ?></li>
+				<li><?php echo $computer_programming_skill_24; ?></li>
+				<li><?php echo $computer_programming_skill_25; ?></li>
+				<li><?php echo $computer_programming_skill_26; ?></li>
+				<li><?php echo $computer_programming_skill_27; ?></li>
+				<li><?php echo $computer_programming_skill_28; ?></li>
+				<li><?php echo $computer_programming_skill_29; ?></li>
+				<li><?php echo $computer_programming_skill_30; ?></li>
+				<li><?php echo $computer_programming_skill_31; ?></li>
+				<li><?php echo $computer_programming_skill_32; ?></li>
+				<li><?php echo $computer_programming_skill_33; ?></li>
+			</ul>
+    </div>
+  </div>
+<?php include 'footer.php'; ?>
