a9664391fc5d4aecb216a8e2bf17efb77cdcaad6
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-02-28 13:24:48 -0600

    Update Website About Me - Work Samples - header.php

--- Website About Me - Work Samples - header.php
+++ Website About Me - Work Samples - header.php
@@ -1,6 +1,6 @@
 <?php
-  // get the data from the navigation
-  $navigation_1 = $_GET['Home'];
+	// get the data from the navigation
+	$navigation_1 = $_GET['Home'];
 	$navigation_2 = $_GET['Hobbies'];
 	$navigation_3 = $_GET['Favorite Music'];
 	$navigation_4 = $_GET['Favorite Bands'];
@@ -16,36 +16,44 @@
 	$navigation_14 = $_GET['Computer Programming Skills'];
 	$navigation_15 = $_GET['Operating Systems'];
 	$navigation_16 = $_GET['Software'];
+	$navigation_17 = $_GET['Favorite Beverages'];
+	$navigation_18 = $_GET['Resources'];
 ?>
 <!DOCTYPE html>
 <html>
-  <!-- the head section -->
-  <head>
-    <link rel="stylesheet" type="text/css" href="Website About Me - php.css" />
-  </head>
-  <!-- the body section -->
-  <body>
-    <div id = "page">
-      <div id = "header">
-        <h1>Work Samples</h1>
-      </div>
-      <div id = "nav">
-        <ul>
-          <li><a href="Website About Me - Main.php"><?php echo $navigation_1; ?></a></li>
-				  <li><a href="Website About Me - Hobbies.php"><?php echo $navigation_2; ?></a></li>
-				  <li><a href="Website About Me - Favorite Music.php"><?php echo $navigation_3; ?></a></li>
-				  <li><a href="Website About Me - Favorite Bands.php"><?php echo $navigation_4; ?></a></li>
-				  <li><a href="Website About Me - Favorite Video Games.php"><?php echo $navigation_5; ?></a></li>
-				  <li><a href="Website About Me - Favorite Gaming Consoles.php"><?php echo $navigation_6; ?></a></li>
-				  <li><a href="Website About Me - Favorite Movies.php"><?php echo $navigation_7; ?></a></li>
-				  <li><a href="Website About Me - Favorite TV Shows.php"><?php echo $navigation_8; ?></a></li>
-				  <li><a href="Website About Me - Favorite Foods.php"><?php echo $navigation_9; ?></a></li>
-				  <li><a href="Website About Me - Favorite Restaurants.php"><?php echo $navigation_10; ?></a></li>
-				  <li><a href="Website About Me - Favorite Stores.php"><?php echo $navigation_11; ?></a></li>
-				  <li><a href="Website About Me - Image Gallery.php"><?php echo $navigation_12; ?></a></li>
-				  <li><a href="Website About Me - Education.php"><?php echo $navigation_13; ?></a></li>
-				  <li><a href="Website About Me - Computer Programming Skills.php"><?php echo $navigation_14; ?></a></li>
-				  <li><a href="Website About Me - Operating Systems.php"><?php echo $navigation_15; ?></a></li>
-				  <li><a href="Website About Me - Software.php"><?php echo $navigation_16; ?></a></li>
-        </ul>
-      </div>
+	<!-- the head section -->
+	<head>
+		<title>Website About Me - Work Samples</title>
+		<link rel="stylesheet" type="text/css" href="Website About Me - php.css" />
+	</head><!-- end head -->
+	<!-- the body section -->
+	<body>
+		<!-- the page section -->
+		<div id = "page">
+			<!-- the header section -->
+			<div id = "header">
+				<h1>Work Samples</h1>
+			</div><!-- end header -->
+			<!-- the nav section -->
+			<div id = "nav">
+				<ul>
+					<li><a href="Website About Me - Main.php"><?php echo $navigation_1; ?></a></li>
+					<li><a href="Website About Me - Hobbies.php"><?php echo $navigation_2; ?></a></li>
+					<li><a href="Website About Me - Favorite Music.php"><?php echo $navigation_3; ?></a></li>
+					<li><a href="Website About Me - Favorite Bands.php"><?php echo $navigation_4; ?></a></li>
+					<li><a href="Website About Me - Favorite Video Games.php"><?php echo $navigation_5; ?></a></li>
+					<li><a href="Website About Me - Favorite Gaming Consoles.php"><?php echo $navigation_6; ?></a></li>
+					<li><a href="Website About Me - Favorite Movies.php"><?php echo $navigation_7; ?></a></li>
+					<li><a href="Website About Me - Favorite TV Shows.php"><?php echo $navigation_8; ?></a></li>
+					<li><a href="Website About Me - Favorite Foods.php"><?php echo $navigation_9; ?></a></li>
+					<li><a href="Website About Me - Favorite Restaurants.php"><?php echo $navigation_10; ?></a></li>
+					<li><a href="Website About Me - Favorite Stores.php"><?php echo $navigation_11; ?></a></li>
+					<li><a href="Website About Me - Image Gallery.php"><?php echo $navigation_12; ?></a></li>
+					<li><a href="Website About Me - Education.php"><?php echo $navigation_13; ?></a></li>
+					<li><a href="Website About Me - Computer Programming Skills.php"><?php echo $navigation_14; ?></a></li>
+					<li><a href="Website About Me - Operating Systems.php"><?php echo $navigation_15; ?></a></li>
+					<li><a href="Website About Me - Software.php"><?php echo $navigation_16; ?></a></li>
+					<li><a href="Website About Me - Favorite Beverages"><?php echo $navigation_17; ?></a></li>
+					<li><a href="Website About Me - Resources.php"><?php echo $navigation_18; ?></a></li>
+				</ul>
+			</div><!-- end nav -->
