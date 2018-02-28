8c6912ed54b11c03e18767ad30e08b34d0b6c755
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-02-28 12:46:13 -0600

    Update Website About Me - Main.php

--- Website About Me - Main.php
+++ Website About Me - Main.php
@@ -1,10 +1,11 @@
 <?php
-  // get the data from the request
-  $first_name = $_GET['Jeff'];
-  $last_name = $_GET['Vogel'];
+	// get the data from the request
+	$first_name = $_GET['Jeff'];
+	$last_name = $_GET['Vogel'];
 ?>
-<?php include 'Website About Me - header.php'; ?>
-  <div class = "content">
-    <p>Hello, My Name is <?php echo $first_name; ?> <?php echo $last_name; ?>. I have high achieving autism.</p>
-  </div>
-<?php include "Website About Me - footer.php'; ?>
+<?php include '../view/header/Website About Me - Main - header.php'; ?>
+	<!-- the content section -->
+	<div class = "content">
+		<p>Hello, My Name is <?php echo $first_name; ?> <?php echo $last_name; ?>. I have high achieving autism.</p>
+	</div><!-- end content -->
+<?php include '../view/Website About Me - footer.php'; ?>
