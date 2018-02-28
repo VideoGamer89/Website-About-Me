9dd46a72357d37f8640b76caaadc33515bf8b35f
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-02-28 13:08:43 -0600

    Update Website About Me - Favorite Gaming Consoles - header.php

--- Website About Me - Favorite Gaming Consoles - header.php
+++ Website About Me - Favorite Gaming Consoles - header.php
@@ -1,35 +1,43 @@
 <?php
-  // get the data from the navigation
-  $navigation_1 = $_GET['Home'];
-  $navigation_2 = $_GET['Hobbies'];
-  $navigation_3 = $_GET['Favorite Music'];
-  $navigation_4 = $_GET['Favorite Bands'];
-  $navigation_5 = $_GET['Favorite Movies'];
-  $navigation_6 = $_GET['Favorite TV Shows'];
-  $navigation_7 = $_GET['Favorite Foods'];
-  $navigation_8 = $_GET['Favorite Restaurants'];
-  $navigation_9 = $_GET['Favorite Video Games'];
-  $navigation_10 = $_GET['Favorite Stores'];
-  $navigation_11 = $_GET['Image Samples'];
-  $navigation_12 = $_GET['Work Samples'];
-  $navigation_13 = $_GET['Education'];
-  $navigation_14 = $_GET['Computer Programming Skills'];
-  $navigation_15 = $_GET['Operating Systems'];
-  $navigation_16 = $_GET['Software'];
+	// get the data from the navigation
+	$navigation_1 = $_GET['Home'];
+	$navigation_2 = $_GET['Hobbies'];
+	$navigation_3 = $_GET['Favorite Music'];
+	$navigation_4 = $_GET['Favorite Bands'];
+	$navigation_5 = $_GET['Favorite Movies'];
+	$navigation_6 = $_GET['Favorite TV Shows'];
+	$navigation_7 = $_GET['Favorite Foods'];
+	$navigation_8 = $_GET['Favorite Restaurants'];
+	$navigation_9 = $_GET['Favorite Video Games'];
+	$navigation_10 = $_GET['Favorite Stores'];
+	$navigation_11 = $_GET['Image Gallery'];
+	$navigation_12 = $_GET['Work Samples'];
+	$navigation_13 = $_GET['Education'];
+	$navigation_14 = $_GET['Computer Programming Skills'];
+	$navigation_15 = $_GET['Operating Systems'];
+	$navigation_16 = $_GET['Software'];
+	$navigation_17 = $_GET['Favorite Beverages'];
+	$navigation_18 = $_GET['Resources'];
 ?>
-<!DOCTYE html>
+<!DOCTYPE html>
 <html>
-  <head>
-    <link href="styles/Website About Me - php.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div id = "page">
-      <div id = "header">
-        <h1>Favorite Gaming Consoles</h1>
-      </div>
-      <div id = "nav">
-        <ul>
-          <li><a href="Website About Me - Main.php"><?php echo $navigation_1; ?></a></li>
+	<!-- the head section -->
+	<head>
+		<link href="styles/Website About Me - php.css" rel="stylesheet" />
+		<title>Website About Me - Favorite Gaming Consoles</title>
+	</head><!-- end head -->
+	<!-- the body section -->
+	<body>
+		<!-- the page section -->
+		<div id = "page">
+			<!-- the header section -->
+			<div id = "header">
+				<h1>Favorite Gaming Consoles</h1>
+			</div><!-- end header -->
+			<!-- the nav section -->
+			<div id = "nav">
+				<ul>
+					<li><a href="Website About Me - Main.php"><?php echo $navigation_1; ?></a></li>
 					<li><a href="Website About Me - Hobbies.php"><?php echo $navigation_2; ?></a></li>
 					<li><a href="Website About Me - Favorite Music.php"><?php echo $navigation_3; ?></a></li>
 					<li><a href="Website About Me - Favorite Bands.php"><?php echo $navigation_4; ?></a></li>
@@ -45,5 +53,7 @@
 					<li><a href="Website About Me - Computer Programming Skills.php"><?php echo $navigation_14; ?></a></li>
 					<li><a href="Website About Me - Operating Systems.php"><?php echo $navigation_15; ?></a></li>
 					<li><a href="Website About Me - Software.php"><?php echo $navigation_16; ?></a></li>
-        </ul>
-      </div>
+					<li><a href="Website About Me - Favorite Beverages.php"><?php echo $navigation_17; ?></a></li>
+					<li><a href="Website About Me - Resources.php"><?php echo $navigation_18; ?></a></li>
+				</ul>
+			</div><!-- end nav -->
