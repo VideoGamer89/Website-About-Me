b67d42688c0e6880b6c29fb02fda7c3260060a47
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-06-19 14:04:46 -0500

    Create Website About Me - Hobbies - mobile.js

---
+++ Website About Me - Hobbies - mobile.js
@@ -0,0 +1,18 @@
+function HobbiesProcess()
+{
+	// Create HTML code
+	var string;
+	string = "<ul>"
+			+ "<li>Playing Video Games</li>"
+			+ "<li>Watching TV</li>"
+			+ "<li>Listening To Music</li>"
+			+ "<li>Watching Movies</li>"
+			+ "<li>Bowling</li>"
+			+ "<li>Going to the <a href=" + "http://www.wisdells.com/wisconsin-dells.htm" + ">Wisconsin Dells</a></li>"
+			+ "<li>Going to the library</td>"
+		+ "</ul>";
+	// obtain a reference to the <div>
+	myDiv = document.getElementById("myDivHobbiesElement");
+	// add content to the <div> element
+	myDiv.innerHTML = string;
+}
